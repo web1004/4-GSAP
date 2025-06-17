42eeaa9e1736514a0e373c5d94c9f1866a214ffb
web1004 <gangsugi@gmail.com>
2025-06-18 08:23:21 +0900

    25.06.18 2차수정

--- 6-practice/5-Y축으로올라오는갤러리/script.js
+++ 6-practice/5-Y축으로올라오는갤러리/script.js
@@ -7,7 +7,7 @@ let tl = gsap.timeline({
   scrollTrigger: {
     trigger: '.gallery',
     pin: true,
-    scrub: 3,
+    scrub: 3,  //부드럽게 진행될수 있도록(1,2,3 중 3으로 제일 부드럽게)
     start: 'top top',
     end: '+=400%', // 시작구간에서 400%까지 스크롤한 후 종료
     markers: true
