76e09e106748ce5096ecca830ae5a581552fe156
web1004 <gangsugi@gmail.com>
2025-06-19 10:19:41 +0900

    25.06.19 수정2

--- 6-practice/10-x축무한반복스크롤과컬러로채워지는텍스트/script.js
+++ 6-practice/10-x축무한반복스크롤과컬러로채워지는텍스트/script.js
@@ -1,5 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
+//(1)비주얼이미지
 gsap.timeline({
   scrollTrigger:{
     trigger:'.visual .imgBox',
@@ -14,11 +15,11 @@ gsap.timeline({
                               {rotationX:0, rotationY:-0, width:'100%', opacity:1},0)
 
 
-
+//(2)컬러로 채워지는 텍스트
 gsap.timeline({
   scrollTrigger:{
     trigger:'.textBox2',
-    pinnedContainer:'.textBox2',
+    pinnedContainer:'.textBox2', //고정할 대상
     start:'0% 60%',
     markers:true,
     scrub:1
